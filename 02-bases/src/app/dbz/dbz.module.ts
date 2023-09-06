00297770da468cabcb7c547fb89bf1ddeb3fd3fa
rodrigo <rodrigo.matto@ayi.group>
2023-09-06 08:37:45 -0300

    Clase-55

---
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  exports: [
+    MainPageComponent
+  ],
+  declarations: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
