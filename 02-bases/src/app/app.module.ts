31ce50022547037a975c68461c6ff91d02dbc97d
rodrigo <rodrigo.matto@ayi.group>
2023-09-06 18:40:28 -0300

    Clase-60

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
-
-import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { DbzModule } from './dbz/dbz.module';
 
+import { AppComponent } from './app.component';
 
 
 @NgModule({
